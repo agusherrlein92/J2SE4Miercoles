8c78794de8557bceedfb4dfb5c2ba7523a753ccf
agusherrlein92 <66702513+agusherrlein92@users.noreply.github.com>
2020-07-15 14:13:36 -0300

    Update Init.java

--- src/main/java/edu/educacionit/Init.java
+++ src/main/java/edu/educacionit/Init.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Init {
     public static void main(String[] args) {
-        Jugador compu = new JugadorConsola("juan");
-        Jugador consola = new JugadorCompu();
+        Jugador compu = new JugadorCompu();
+        Jugador consola = new JugadorConsola("Juan");
         
         Juego juego = new Juego(compu, consola);
         juego.jugar();
